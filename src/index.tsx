ef4d0fc840a611c4243e91887347e46990a00efa
Rebecca Reed <rreed@and.digital>
2020-08-12 16:45:17 +0100

    Add in normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { App } from 'containers';
-import { ThemeProvider } from 'react-jss';
+import { ThemeProvider, jss } from 'react-jss';
 import themes from 'theme/themes';
 import { Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import history from 'routes/history';
 import { Provider } from 'react-redux';
 import store from 'store';
+import normalize from 'normalize-jss';
+
+jss.createStyleSheet(normalize).attach();
 
 ReactDOM.render(
   <Provider store={store}>
